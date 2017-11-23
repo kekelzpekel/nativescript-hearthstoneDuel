74f5f99b9c2a96afc38cdc1cdea6b29d7efc0674
Lukas Hlobil <lukashlobil@users.noreply.github.com>
2017-11-23 18:23:15 +0100

    Arena draft, base64 (#3)

--- app/services/heroList.ts
+++ app/services/heroList.ts
@@ -6,47 +6,56 @@ export class HeroList {
         {
             name: 'druid',
             id: 0,
-            src: 'druid'
+            src: 'druid',
+            dbfId: 274,
         },
         {
             name: 'hunter',
             id: 1,
-            src: 'hunter'
+            src: 'hunter',
+            dbfId: 31,
         },
         {
             name: 'mage',
             id: 2,
-            src: 'mage'
+            src: 'mage',
+            dbfId: 637,
         },
         {
             name: 'paladin',
             id: 3,
-            src: 'paladin'
+            src: 'paladin',
+            dbfId: 671,
         },
         {
             name: 'priest',
             id: 4,
-            src: 'priest'
+            src: 'priest',
+            dbfId: 813,
         },
         {
             name: 'rogue',
             id: 5,
-            src: 'rogue'
+            src: 'rogue',
+            dbfId: 930,
         },
         {
             name: 'shaman',
             id: 6,
-            src: 'shaman'
+            src: 'shaman',
+            dbfId: 1066,
         },
         {
             name: 'warlock',
             id: 7,
-            src: 'warlock'
+            src: 'warlock',
+            dbfId: 893,
         },
         {
             name: 'warrior',
             id: 8,
-            src: 'warrior'
+            src: 'warrior',
+            dbfId: 7,
         }
     ];
 
